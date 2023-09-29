6c7d25e40f3cf16acf18ef8cba7761e448f1e88d
myteam123223 <146390286+myteam123223@users.noreply.github.com>
2023-09-29 18:31:46 +0200

    Update utils.ts

--- sendVideo/utils.ts
+++ sendVideo/utils.ts
@@ -3,11 +3,10 @@ const variables = {
 	hostsDown: null,
 	filter: "VIDEO",
 	limit: 10,
-	subreddit: "blowjobs" // Agrega el nombre del subreddit aquí
 }
 
 const qrDiscoverSubreddits = `
-query DiscoverSubredditsQuery( $filter: MediaFilter $limit: Int $iterator: String $subreddit: String ) { discoverSubreddits( isNsfw: true filter: $filter limit: $limit iterator: $iterator subreddit: $subreddit ) { iterator items { __typename id url title secondaryTitle description createdAt isNsfw subscribers isComplete itemCount videoCount pictureCount albumCount isPaid username tags banner { url width height isOptimized } isFollowing children( limit: 2 iterator: null filter: SOUND disabledHosts: null homePage: true ) { iterator items { __typename id url title subredditId subredditTitle subredditUrl redditPath isNsfw albumUrl hasAudio fullLengthSource gfycatSource redgifsSource ownerAvatar username displayName isPaid tags isFavorite mediaSources { url width height isOptimized } blurredMediaSources { url width height isOptimized } } } } } } 
+query DiscoverSubredditsQuery( $filter: MediaFilter $limit: Int $iterator: String ) { discoverSubreddits( isNsfw: true filter: $filter limit: $limit iterator: $iterator ) { iterator items { __typename id url title secondaryTitle description createdAt isNsfw subscribers isComplete itemCount videoCount pictureCount albumCount isPaid username tags banner { url width height isOptimized } isFollowing children( limit: 2 iterator: null filter: SOUND disabledHosts: null homePage: true ) { iterator items { __typename id url title subredditId subredditTitle subredditUrl redditPath isNsfw albumUrl hasAudio fullLengthSource gfycatSource redgifsSource ownerAvatar username displayName isPaid tags isFavorite mediaSources { url width height isOptimized } blurredMediaSources { url width height isOptimized } } } } } } 
 `
 
 export async function getData(): Promise<any> {
@@ -34,6 +33,10 @@ export interface Video {
 	playUrl: string
 	cover: string
 	caption: string
+}
+
+export interface Media {
+	url: string
 }
 
 export interface Media {
