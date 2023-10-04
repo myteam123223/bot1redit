94e65e76c2d8a3afc01e735a17b12a33b942e9d8
myteam123223 <146390286+myteam123223@users.noreply.github.com>
2023-10-05 00:48:56 +0200

    Update send-video.ts

--- netlify/functions/send-video.ts
+++ netlify/functions/send-video.ts
@@ -1,12 +1,12 @@
 import { schedule } from "@netlify/functions"
 import { sendVideos } from "../../sendVideo/index"
-import { N_VIDEOS, SUBREDDIT_NAME  } from "../../sendVideo/myConfig"
+import { N_VIDEOS } from "../../sendVideo/myConfig"
 
-let frequency = "0 */1 * * *"
+let frequency = "0 */2 * * *"
 
 export const handler = schedule(frequency, async () => {
 	console.log("Handler Init...")
-	await sendVideos(N_VIDEOS, SUBREDDIT_NAME)
+	await sendVideos(N_VIDEOS)
 	console.log("Handler Finish...")
 	return {
 		statusCode: 200,
